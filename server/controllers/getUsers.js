c66e6d7c3445bdad942bcaa72c307b5a21cc09d9
Shivam Kumar Kashyap <109240141+MernWala@users.noreply.github.com>
2025-02-03 19:19:31 +0530

    Code refactor; conditional reference capture;

--- server/controllers/getUsers.js
+++ server/controllers/getUsers.js
@@ -2,12 +2,16 @@ const userModel = require("../models/userModel");
 
 const getUser =async (req,res) =>{
     try {
-        const loggedInUserId = req.body.user.id;
-        const filteredUsers = await userModel.find({ _id: { $ne: loggedInUserId } }).select("-password");
+        const loggedInUserId = req?.body?.user?.id;
+        let filteredUsers
+
+        if(loggedInUserId){
+          filteredUsers = await userModel.find({ _id: { $ne: loggedInUserId } }).select("-password");
+        }
     
         res.status(200).json({success:true,filteredUsers});
       } catch (error) {
-        console.error("Error in getUsersForSidebar: ", error.message);
+        console.log("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({success:false ,message: "Internal server error" });
       }
 }
